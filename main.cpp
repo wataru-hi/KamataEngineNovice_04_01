5af7a089cb87c7ce9ec2373d9788cac57a512f47
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-02-12 11:17:13 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <Novice.h>
+#include "GameManager.h"
+
+const char kWindowTitle[] = "ヒガ_ワタル";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// 生成
+	GameManager* gameManager = new GameManager();
+
+	// ゲーム実行
+	gameManager->Run();
+
+	// 解放
+	delete gameManager;
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
